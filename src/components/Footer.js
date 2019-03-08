18bb7f847aabc42bb28d459bb934be69ca070ad5
Sam Smiley <samtsmiley@gmail.com>
2019-03-08 12:47:12 -0800

    smiley face features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,10 @@ import React from 'react'
 import './Footer.css'
 
 export default () => {
-    return (
-      <div className='footer'>
-        <p>Click the Smiley Face for more ;)</p>
-        <p>Thanks For Stopping By</p>
-        <p>Have a Great Day</p>
-      </div>
-    )
+  return (
+    <div className='footer'>
+      <p>Click the Smiley Face for more ;)</p>
+      <p>Thanks For Stopping By, Have a Great Day!</p>
+    </div>
+  )
 }
