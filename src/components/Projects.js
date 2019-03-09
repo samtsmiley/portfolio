e30fa2360d3e2b24d82371415bbc94eb9e430aa0
Sam Smiley <samtsmiley@gmail.com>
2019-03-09 12:09:53 -0800

    changed projects order

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,28 +10,6 @@ export class Projects extends React.Component {
   render() {
     const projectData = [
       {
-        title: 'SLP Student Tracker',
-        desc: 'SLP Student Tracker is an app made for Speech Language Pathologists to keep track of students by recording a SOAP note. With a history of notes, a visual graph will show students progress.',
-        tech: 'React, Redux, Node, Express, Mocha/Chai, and Mongo',
-        me: 'All of it. I am in the process of refactoring this app using React Hooks for state management.',
-        colab: 'Just Me',
-        displayLeft: true,
-        image: slpst,
-        link: 'https://slpst.herokuapp.com/',
-        git: 'https://github.com/thinkful-ei26/sam-sst-client'
-      },
-      {
-        title: 'DothraKit',
-        desc: 'With the final season of GOT coming out it\'s time to brush up on your Dothraki phrases! DothraKIT uses a spaced repetition algorithm to ensure you are learning efficiently. The rules are simple: learn the word, get it right, you live. Or else...well, best not to get on Khal\'s bad side. You can log in with username: Sammy, password: 0123456789.',
-        tech: 'React, Redux, Node, Express, Mocha/Chai, Enzyme, and Mongo',
-        me: 'With just one partner on this project, we shared a lot of the responsibilities.',
-        displayLeft: false,
-        colab: 'Nikkie Mash',
-        image: dothrakit,
-        link: 'https://dothrakit.herokuapp.com/dashboard',
-        git: 'https://github.com/thinkful-ei26/DothraKIT-Client-Nikkie-Sam'
-      },
-      {
         title: 'Game-A-Tron',
         desc: '   Game-A-Tron is a web application that helps people to gamify their lives. Players can create and play games. It was a collaborative project with four of us.',
         tech: 'React, Redux, Node, Express, Mocha/Chai, Enzyme and Mongo',
@@ -42,6 +20,30 @@ export class Projects extends React.Component {
         link: 'https://ei26.netlify.com/',
         git: 'https://github.com/open-sourcerers-ei26/client'
       }
+     ,
+      {
+        title: 'DothraKit',
+        desc: 'With the final season of GOT coming out it\'s time to brush up on your Dothraki phrases! DothraKIT uses a spaced repetition algorithm to ensure you are learning efficiently. The rules are simple: learn the word, get it right, you live. Or else...well, best not to get on Khal\'s bad side. You can log in with username: Sammy, password: 0123456789.',
+        tech: 'React, Redux, Node, Express, Mocha/Chai, Enzyme, and Mongo',
+        me: 'With just one partner on this project, we shared a lot of the responsibilities.',
+        displayLeft: false,
+        colab: 'Nikkie Mash',
+        image: dothrakit,
+        link: 'https://dothrakit.herokuapp.com/dashboard',
+        git: 'https://github.com/thinkful-ei26/DothraKIT-Client-Nikkie-Sam'
+      }, 
+      {
+        title: 'SLP Student Tracker',
+        desc: 'SLP Student Tracker is an app made for Speech Language Pathologists to keep track of students by recording a SOAP note. With a history of notes, a visual graph will show students progress.',
+        tech: 'React, Redux, Node, Express, Mocha/Chai, and Mongo',
+        me: 'All of it. I am in the process of refactoring this app using React Hooks for state management.',
+        colab: 'Just Me',
+        displayLeft: true,
+        image: slpst,
+        link: 'https://slpst.herokuapp.com/',
+        git: 'https://github.com/thinkful-ei26/sam-sst-client'
+      }
+      
     ]
 
     const projects = projectData.map((project, index) => {
